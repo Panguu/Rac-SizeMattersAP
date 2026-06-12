10fa1080a91291a9412db19314cebf16f7eaa7af
AmondoVagyok <solokapitany10@gmail.com>
2026-06-12 22:44:10 +0200

    SM: items, planets, skillpoint constants files

---
+++ constants/planets.py
@@ -0,0 +1,14 @@
+"""This module contains string constants used for planets"""
+
+class RACSMPLANET:
+    """String constants for each planet"""
+    POKITARU = "POKITARU"
+    RYLLUS = "RYLLUS"
+    KALIDON = "KALIDON"
+    METALIS = "METALIS"
+    DREAMTIME = "DREAMTIME"
+    OUTPOST_OMEGA = "OUTPOST_OMEGA"
+    CHALLAX = "CHALLAX"
+    DAYNI_MOON = "DAYNI_MOON"
+    INSIDE_CLANK = "INSIDE_CLANK"
+    QUODRONA = "QUODRONA"
