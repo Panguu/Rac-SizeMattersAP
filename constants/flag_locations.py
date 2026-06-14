7f7bc3be936c6cd316137bde19f89429496035a5
AmondoVagyok <solokapitany10@gmail.com>
2026-06-14 13:38:36 +0200

    SM: Add remaining locations

---
+++ constants/flag_locations.py
@@ -0,0 +1,13 @@
+"""This module contains string constants for each flag related location"""
+
+class RACSMFLAGLOCATION:
+    """String constants for flag locations"""
+    RYLLUS_BUZZING = "Ryllus: Buzzing Cameras"
+    KALIDON_EXPLORE = "Kalidon: Explore the planet"
+    METALIS_CLANK = "Metalis: Start Giant Clank"
+    OUTPOST_OMEGA = "Outpost Omega: Escape from facility pt 1"
+    DAYNI_MOON_FIGHT1 = "Dayni Moon: Luna fight pt 1"
+    DAYNI_MOON_FIGHT2 = "Dayni Moon: Luna fight pt 2"
+    QUODRONA_CLONE = "Quodrona: Clone Wars (Fight the Ratchet Clones)"
+    QUODRONA_CHASE = "Quodrona: Runnnn from Otto"
+    QUODRONA_MECHA = "Quodrona: Defeat Mecha Otto"
